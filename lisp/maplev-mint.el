0834859b6f749850f5050da60f9067d6b3e1d24e
Joe Riel <jriel@maplesoft.com>
2018-07-09 22:49:38 -0700

    Treat "used as globals but don't start with _" as undeclared globals.

--- lisp/maplev-mint.el
+++ lisp/maplev-mint.el
@@ -380,6 +380,7 @@ REPLACE is an alist with elements \(OLD . NEW\)."
     ;;("These local variables were assigned a value, but otherwise unused:" ... )
     ("These names were declared more than once as a local variable:" maplev-mint-warning-face 'repeat-local t)
     ("These names were used as global names but were not declared:" maplev-mint-warning-face 'undecl-global t)
+    ("These names were used as global names but the names don't start with _:" maplev-mint-warning-face 'undecl-global t)
     ("\\(on line +[0-9]+\\)" maplev-mint-link-face 'goto-line)
    ; ("\\(on lines +[0-9]+\\s-+to\\s-++[0-9]+\\s-+of\\s-+.*\\)" maplev-mint-note-face 'goto-line)
     )
