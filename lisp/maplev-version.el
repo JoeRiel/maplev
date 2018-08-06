164b845a2f2fe744aafeb90ef6d5e468ad1e7255
Joe Riel <jriel@maplesoft.com>
2018-08-06 14:43:35 -0700

    prepare release

--- lisp/maplev-version.el
+++ lisp/maplev-version.el
@@ -1,6 +1,6 @@
 ;;; maplev-version.el --- auto-generated file, do not edit.
 (defun maplev-release () "Return maplev-release." "3.0")
-(defun maplev-git-version () "Return maplev-git-version." "release-2.36-259-g49ae7c")
+(defun maplev-git-version () "Return maplev-git-version." "release-2.36-262-gb9af64")
 (provide 'maplev-version)
 ;; Local Variables:
 ;; version-control: never
